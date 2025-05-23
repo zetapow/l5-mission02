632c6ac510b7be97996fb8d5d730139f5b4c2474
Peter Zhou <zetapow@gmail.com>
2025-05-24 10:59:36 +1200

    feat: start on calcualting discount rate

--- backend/server.js
+++ backend/server.js
@@ -14,13 +14,17 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
 
-// Prediction API endpoint
-app.post("/predict", predictionController.predictImage);
+// API1: Car value API
+
+// API2: Risk rating API
+
+// API3: Insurance quote API
+
+// API4: Discount API endpoint
+// app.post("/apis/discount");
 
 // Handle errors
-app.use(handleErrors);
 
 app.listen(config.port, () => {
    console.log(`Server running on http://localhost:${config.port}`);
-   // console.log("Custom Vision Configuration:", config.vision);
 });
